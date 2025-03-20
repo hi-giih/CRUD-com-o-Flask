9f58fa46da5f118411e6d12d8606423f903cb1b3
Giovanna Santos <gihsantos494@gmail.com>
2025-03-20 16:22:32 -0300

    Subindo Projeto

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# CRUD-com-o-Flask
+# CRUD com Flask
+![Static Badge](https://img.shields.io/badge/status-Active-gren?style=for-the-badge)
+
+## 📄 Descrição
+
+Este projeto é um exemplo simples de CRUD (Create, Read, Update, Delete) utilizando o microframework Flask em Python. Ele permite criar, visualizar, atualizar e excluir tarefas através de requisições HTTP. Esse projeto foi desenvolvido na aula de **Desenvolvimento de APIs com Flask** 
+
+## ⚙️ Funcionalidades
+
+- **Criar Tarefa:**  Adiciona uma nova tarefa à lista.
+- **Visualizar Todas as Tarefas:** Retorna todas as tarefas cadastradas.
+- **Visualizar uma Tarefa Específica:** Exibe os detalhes de uma tarefa pelo seu ID.
+- **Atualizar Tarefa:** Modifica as informações de uma tarefa existente.
+- **Deletar Tarefa:** Remove uma tarefa da lista.
+
+## 💻 Tecnologias Utilizadas
+
+- **Python 3.11**: Linguagem utilizada para o desenvolvimento do jogo.
+- **Flask 2.3.0**: Framework de desenvolvimento web utilizado para criar a aplicação e gerenciar rotas HTTP.
+
+## 🚀 Instalando e Rodando o Projeto
+
+1. Clone este repositório: `git@github.com:hi-giih/JogoAdvinhacao.git`
+2. Acesse o diretório do projeto: `cd desafio2  - POO - Jogo Advinhação`
+3. Crie um ambiente virtual (opcional, mas recomendado):
+
+          python -m venv venv
+
+4. Ative o ambiente virtual:
+
+        Windows: venv\Scripts\activate
+
+        Linux/Mac: source venv/bin/activate
+
+5. Instale as dependências:
+
+        pip install flask
+
+6. Execute o projeto:
+
+        python app.py
+
+    O servidor estará disponível em: http://127.0.0.1:5000
+
+## 🔧 Futuras Melhorias
+
+- Implementação de persistência de dados com um banco de dados (SQLite, PostgreSQL, etc.).
+- Autenticação e autorização para maior segurança.
+- Validações mais robustas nas requisições.
+- Interface gráfica com HTML/CSS para interação com o usuário.
+
+## 📜 Licença 
+
+Este projeto não está sob nenhuma licença.
---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+#Post
+@app.route('/tasks', methods=["POST"])
+
+def create_tasks():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ""))
+    task_id_control +=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message":"Nova tarefa criada com sucesso"})
+
+#GET
+#Todas as tarefas
+@app.route('/tasks', methods=["GET"])
+
+def get_tasks():
+    task_lists = [ task.to_dict() for task in tasks]
+    
+    output = {
+            "tasks": task_lists,
+            "total_tasks":len(task_lists)
+            }
+    return jsonify(output)
+
+#Apenas uma atividade
+@app.route('/tasks/<int:id>', methods=["GET"])
+
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    
+    return jsonify({"message": "Não foi possivel encontrar a tarefa"}), 404
+
+#PUT
+@app.route('/tasks/<int:id>', methods=["PUT"])
+
+def update_task(id):
+    task = None
+    print(task)
+    for t in tasks:
+        if t.id == id:
+            task=t
+            break
+
+        if task == None:
+            return jsonify({"message": "Não foi possivel encontrar a tarefa"}), 404
+
+        data = request.get_json()
+        task.title = data['title']
+        task.description = data['description']
+        task.completed = data['completed']
+        return jsonify({"message": "Tarefa atualizada com sucesso"})
+        
+    print(task)
+
+#DELETE
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    if not task:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
+if __name__=='__main__':
+    app.run()
